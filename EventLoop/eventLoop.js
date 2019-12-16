dda3cfe0d8d12967a6f46dd363d32239ad220cf5
Jeongho Noh <nossi8128@gmail.com>
2019-12-16 15:33:44 +0900

    added eventloop-result2

--- EventLoop/eventLoop.js
+++ EventLoop/eventLoop.js
@@ -1,4 +1,4 @@
-/////////////// result 1
+/////////////////////// result1
 // const fs = require('fs');
 
 // setTimeout(() => {
@@ -12,9 +12,8 @@
 // });
 // console.log('top-level code');
 
-//////////////////result 2
+///////////////////// result2
 const fs = require('fs');
-
 setTimeout(() => {
   console.log('Expired timer callbacks');
 }, 0);
@@ -22,6 +21,32 @@ setImmediate(() => {
   console.log('setImediate callbacks');
 });
 fs.readFile('./test.txt', () => {
-  console.log('I/O polling and callbacks');
+  console.log('I/O polling and callbacks 1');
+  console.log('------------------');
+
+  setTimeout(() => {
+    console.log('setTimeout 1-1');
+  }, 0);
+  setTimeout(() => {
+    console.log('setTimeout 1-2');
+  }, 3000);
+  setImmediate(() => {
+    console.log('setImediate 1');
+  });
+});
+
+fs.readFile('./test.txt', () => {
+  console.log('I/O polling and callbacks 2');
+  console.log('------------------');
+
+  setTimeout(() => {
+    console.log('setTimeout 2-1');
+  }, 0);
+  setTimeout(() => {
+    console.log('setTimeout 2-2');
+  }, 3000);
+  setImmediate(() => {
+    console.log('setImediate 2');
+  });
 });
 console.log('top-level code');
